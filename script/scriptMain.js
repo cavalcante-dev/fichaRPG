7dce6bfaab5ab7bc16055634914c9dce4d5de8a5
cavalcante-dev <leandrocavalcante2504@gmail.com>
2025-07-10 21:59:37 -0300

    feat: cria um popup para spells e as adiciona na lista

--- script/scriptMain.js
+++ script/scriptMain.js
@@ -1,20 +1,78 @@
-function openSpellList() {
 
-    const varWindow = window.open (
-        'spellList.html', 
-        'popup',
-        "width=350, heigth=255, scrollbars=no, resizable=no, location=no, toolbar=no, menubar=no");
+// function for opening all the modals
+const openButtons = document.querySelectorAll('.openSpellModal');
 
-}
+openButtons.forEach(button => {
+    button.addEventListener('click', () => {
 
+        const modalId = button.getAttribute('data-modal');
+        const modal = document.getElementById(modalId);
+        
+        modal.showModal();
 
-function addSpell() {
+    });
+});
 
-    const spellListItem = document.getElementById("spellListItem");
-    let newSpell = document.createElement("li");
+// function for closing all the modals
+const closeButtons = document.querySelectorAll('.closeSpellModal');
 
-    newSpell.textContent = "Nova Magia";
+closeButtons.forEach(button => {
+    button.addEventListener('click', () => {
 
-    spellListItem.appendChild(newSpell);
+        const modalId = button.getAttribute('data-modal');
+        const modal = document.getElementById(modalId);
+
+        modal.close();
+
+    });
+});
+
+// function for adding a spell into the known spell list
+
+const spellName = {
+    "maosMagicas": "Mão Mágicas",
+    "protecaoContraLaminas": "Proteção Contra Láminas" 
+};
+
+const addButtons = document.querySelectorAll('.addSpell');
+const cantripsList = document.getElementById("cantripsList");
+
+addButtons.forEach(button => {
+    button.addEventListener('click', () => {
+
+        const spellId = button.getAttribute('data-spell');
+        renderSpellList(spellId);
+
+    });
+});
+
+function renderSpellList(spellId) {
+
+    if (spellName[spellId]) {
+
+        const divNewSpell = document.createElement("div");
+        divNewSpell.className = "knownSpell";
+        const newSpell = document.createElement("li");
+        const hr = document.createElement("hr");
+        hr.className = "separator";
+        
+        let descriptionButton = document.createElement("button");
+        descriptionButton.className = "description";
+        descriptionButton.textContent=">";
+        descriptionButton.onclick = () => seeDescription(spellId[spellId]);
+
+        let removeButton = document.createElement("button");
+        removeButton.className = "remove";
+        removeButton.textContent="X";
+        removeButton.onclick = () => removeSpell(spellName[spellId]);
+
+        newSpell.textContent = spellName[spellId];
+        
+        cantripsList.appendChild(divNewSpell);
+        cantripsList.appendChild(newSpell);
+        cantripsList.appendChild(descriptionButton);
+        cantripsList.appendChild(removeButton);
+
+    }
 
 }
--- script/scriptSpellList.js
+++
@@ -1,7 +0,0 @@
-function addSpellOnMain() {
-    if (window.opener&& !window.opener.closed) {
-        window.opener.addSpell();
-    } else {
-        alert('A janela principal ta fechada seu pamonha')
-    }
-}
